9c4589549e66f01e37a31c4cfed156957f325a17
Alexander Christopher Kepas Brown <abrown96@newmexico.cs.byu.edu>
2022-09-23 15:54:31 -0600

    finish lab 1

--- lab-link-layer/switch.py
+++ lab-link-layer/switch.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 import asyncio
+from sys import byteorder
 import time
+import binascii
+
 from cougarnet.sim.host import BaseHost
 
 class Switch(BaseHost):
@@ -13,40 +16,122 @@ class Switch(BaseHost):
         # do any initialization here...
 
     def _handle_frame(self, frame, intf):
-        print('Received frame: %s' % repr(frame))
+        print("Start")
+        # print('Received frame: %s\n' % repr(frame))
 
         #purge old entries
         self._purge_table()
 
-        #check for Trunk Protocol: (if):
-        if False:
-            pass
-            #grab VLAN group
-            #Strip trunk protocol
+        #trunk protocol overhead
+        vlan = self._get_vlan_from_frame(frame)
+        frame = self._strip_trunk_protocol_from_frame(frame)
         
         #extract destination and source from frame
         dest = frame[0:6]
         source = frame[6:12]
 
+        if vlan == -1:
+            vlan = self.int_to_info[intf].vlan
+
+        # if not(self._is_dest_in_vlan(dest, vlan)):
+        #     print("drop packet")
+        #     return
+
+        #update table with source
+        self._add_source_to_table(source, intf)
+
+        # print("frame (s): %s'" % repr(frame))
+        print("vlan:      %s" % vlan)
+        print("dest:      %s" % repr(dest))
+        print("source:    %s\n" % repr(source))
+
+        if dest == b'\xff\xff\xff\xff\xff\xff':
+            print("Brodcast")
+
+            for all_intf in self.physical_interfaces:
+                if all_intf == intf:
+                    continue
+
+                print("source: ", repr(source), " with VLAN: ", self.int_to_info[intf].vlan)
+                print("dest:   ", repr(dest),   " with VLAN: ", self.int_to_info[all_intf].vlan)
+
+                self._send_frame_to_intf(frame, all_intf, vlan)
+
         #check table if destination exists (if):
-        if self._is_dest_in_table(dest):
-            #update table with source
-            self._add_source_to_table(source, intf)
+        elif self._is_dest_in_table(dest):
+            print("Found match in table!\n")
             
             #send frame to interface from table
             selected_intf = self.macAddressTable[dest][0]
 
-        else:
-            #add to table with soruce
-            self._add_source_to_table(source, intf)
+            # if vlan == -1:
+            #     vlan = self.int_to_info[selected_intf].vlan
+            #     print("intf: ", selected_intf, " vlan: ", vlan)
 
-            
+            print("source: ", repr(source), " with VLAN: ", self.int_to_info[intf].vlan)
+            print("dest:   ", repr(dest),   " with VLAN: ", self.int_to_info[selected_intf].vlan)
+
+            self._send_frame_to_intf(frame, selected_intf, vlan)
+
+        else:
+            print("Did not find a match in the table!\n")
+
             #send frame to all unknown interfaces
-                #check if sending over trunk (if):
-                    #add trunk protocol
+            for unknown_intf in self.physical_interfaces:
+                # if vlan == -1:
+                #     n_vlan = self.int_to_info[unknown_intf].vlan
+                #     print("intf: ", unknown_intf, " vlan: ", vlan)
+
+                #ignore known and sending intfaces
+                if self._is_intf_in_table(unknown_intf) or unknown_intf == intf:
+                    continue
+
+                print("source: ", repr(source), " with VLAN: ", self.int_to_info[intf].vlan)
+                print("dest:   ", repr(dest),   " with VLAN: ", self.int_to_info[unknown_intf].vlan)
+                #send frame to interface from table
+                self._send_frame_to_intf(frame, unknown_intf, vlan)
+        
+        print("Done\n\n\n")
+
+
+
+    def _send_frame_to_intf(self, frame, intf, vlan):
+        if vlan != self.int_to_info[intf].vlan and self.int_to_info[intf].vlan != -1:
+            print("drop packet")
+            return
+
+        if (self._is_trunk_link(intf)):
+            #add trunk protocol
+            print("hit construct")
+            frame = self._construct_trunk_protocol(frame, vlan)
+                
+        # print("intf: %s" % intf)
+        print("sending frame: %s\n" % repr(frame))
+        self.send_frame(frame, intf)
+
+    def _construct_trunk_protocol(self, frame, vlan):
+        # print("vlan: ", vlan)
+        assert vlan != -1
+        vbytes = vlan.to_bytes(2, byteorder='big')
+        b = b'\x81\x00' + vbytes
+        # print(binascii.hexlify(b).decode('latin1'))
+        return frame[0:12] + b + frame[12:]
+
+    def _strip_trunk_protocol_from_frame(self, frame):
+        if frame[12:14] == b'\x81\x00':
+            return frame[0:12] + frame[16:]
+        else:
+            return frame
+
+    def _get_vlan_from_frame(self, frame):
+        if frame[12:14] == b'\x81\x00':
+            # print("got trunk header")
+            return int.from_bytes(frame[14:16], byteorder='big') 
+        else:
+            return -1
 
     def _purge_table(self):
-        for k in self.macAddressTable.keys():
+        for k in list(self.macAddressTable.keys()):
             ttl = self.macAddressTable[k][1]
 
             if ttl < time.time():
@@ -55,9 +140,26 @@ class Switch(BaseHost):
     def _is_dest_in_table(self, dest):
         return dest in self.macAddressTable.keys()
 
+    def _is_dest_in_vlan(self, dest, vlan):
+        if not (self._is_dest_in_table(dest)):
+            return False
+        intf = self.macAddressTable[dest][0]
+        intfInfo = self.int_to_info[intf]
+        if vlan == intfInfo.vlan:
+            return True
+        elif intfInfo.vlan == -1:
+            return True
+        return False
+
     def _add_source_to_table(self, source, intf):
         ttl = time.time() + 8
         self.macAddressTable[source] = (intf, ttl)
+
+    def _is_intf_in_table(self, intf):
+        for value in self.macAddressTable.values():
+            if intf in value:
+                return True
+        return False
 
 def main():
     with Switch() as switch:
