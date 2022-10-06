1393f4894241c0bd5e824c89542971964c46d793
Alexander Christopher Kepas Brown <abrown96@rhodeisland.cs.byu.edu>
2022-10-05 18:13:22 -0600

    part 1

--- lab-network-layer/host.py
+++ lab-network-layer/host.py
@@ -6,6 +6,8 @@ import os
 import socket
 import sys
 
+from numpy import source
+
 from cougarnet.sim.host import BaseHost
 from cougarnet.util import \
         mac_str_to_binary, mac_binary_to_str, \
@@ -31,11 +33,51 @@ class Host(BaseHost):
         super(Host, self).__init__()
 
         self._ip_forward = ip_forward
+        self._arp_table = {}
+        self._queued_pkts = {}
 
         # do any additional initialization here
 
+    def is_ip_in_arp_table(self, ip):
+        return ip in self._arp_table.keys()
+
+    def set_queued_pkt(self, ip, pkt):
+        if not (ip in self._queued_pkts):
+            self._queued_pkts[ip] = []
+        self._queued_pkts[ip].append(pkt)
+        
+
+    def get_queued_pkts(self, ip):
+        if ip in self._queued_pkts:
+            return self._queued_pkts.pop(ip)
+            
+
+    def send_arp_message(self, s_ip, s_mac, d_ip, d_mac, opcode, intf):
+        request = b'\x00\x01' + b'\x08\x00' + b'\x06' + b'\x04' + opcode + s_mac + s_ip + d_mac + d_ip
+
+        dest_mac = b'0'
+        if (opcode == b'\x00\x01'):
+            dest_mac = mac_str_to_binary("ff:ff:ff:ff:ff:ff")
+        else:
+            dest_mac = d_mac
+
+        frame = dest_mac + s_mac + b'\x08\x06' + request
+
+        self.send_frame(frame, intf)
+        print("sent arp frame\n")
+
+
     def _handle_frame(self, frame, intf):
-        pass
+        print("recieved frame: ", repr(frame))
+        dest_mac = mac_binary_to_str(frame[0:6])
+        if (dest_mac == "ff:ff:ff:ff:ff:ff" or dest_mac == self.int_to_info[intf].mac_addr):
+            e_type = frame[12:14]
+            if (e_type ==  b'\x08\x00'):
+                self.handle_ip(frame[14:], intf)
+            elif (e_type == b'\x08\x06'):
+                self.handle_arp(frame[14:], intf)
+
+
 
     def handle_ip(self, pkt, intf):
         pass
@@ -47,16 +89,95 @@ class Host(BaseHost):
         pass
 
     def handle_arp(self, pkt, intf):
-        pass
+        print("getting an arp packet: ", repr(pkt))
+        opcode = pkt[6:8]
+        if (opcode == b'\x00\x01'):
+            self.handle_arp_request(pkt, intf)
+        else:
+            self.handle_arp_response(pkt, intf)
 
     def handle_arp_response(self, pkt, intf):
-        pass
+        print("was arp response")
+        sender_mac = mac_binary_to_str(pkt[8:14])
+        sender_ip  = ip_binary_to_str(pkt[14:18])
+
+        print("sender ip: ", sender_ip, ", sender mac: ", sender_mac)
+        
+        self._arp_table[sender_ip] = sender_mac
+
+        queued_pkts = self.get_queued_pkts(sender_ip)
+
+        for pkt in queued_pkts:
+            d_mac = mac_str_to_binary(sender_mac)
+            s_mac = mac_str_to_binary(self.int_to_info[intf].mac_addr)
+            e_type = b'\x08\x00'
+            
+            frame = d_mac + s_mac + e_type + pkt
+
+            self.send_frame(frame, intf)
+            print("sent frame")
+
+        print("")
+            
+
 
     def handle_arp_request(self, pkt, intf):
-        pass
+        print("was arp request")
+        sender_mac = mac_binary_to_str(pkt[8:14])
+        sender_ip  = ip_binary_to_str(pkt[14:18])
+
+        dest_ip  = ip_binary_to_str(pkt[24:28])
+
+        print("sender ip: ", sender_ip, ", sender mac: ", sender_mac)
+        
+        self._arp_table[sender_ip] = sender_mac
+
+        if (dest_ip == self.int_to_info[intf].ipv4_addrs[0]):
+            opcode = b'\x00\02'
+            s_mac = mac_str_to_binary(self.int_to_info[intf].mac_addr)
+            s_ip  = ip_str_to_binary(dest_ip)
+            d_mac = mac_str_to_binary(sender_mac)
+            d_ip  = ip_str_to_binary(sender_ip)
+
+            self.send_arp_message(s_ip, s_mac, d_ip, d_mac, opcode, intf)
+        else:
+            print("lol not me drop\n")
 
     def send_packet_on_int(self, pkt, intf, next_hop):
         print(f'Attempting to send packet on {intf} with next hop {next_hop}:\n{repr(pkt)}')
+        
+        if self.is_ip_in_arp_table(next_hop):
+            print("hit in table")
+            dest_mac = mac_str_to_binary(self._arp_table[next_hop])
+            source_mac = mac_str_to_binary(self.int_to_info[intf].mac_addr)
+            type_ip = b'\x08\x00'
+
+            print("dest_mac:   ", dest_mac, " ", type(dest_mac))
+            print("source_mac: ", source_mac, " ", type(source_mac))
+            print("type_ip:    ", type_ip, " ", type(type_ip))
+            print("payload:    ", pkt, " ", type(pkt))
+
+            frame = dest_mac + source_mac + type_ip + pkt
+            self.send_frame(frame, intf)
+            print("sent frame\n")
+
+            
+        else:
+            print("no hit in table")
+            self.set_queued_pkt(next_hop, pkt)
+            source_ip  = ip_str_to_binary(self.int_to_info[intf].ipv4_addrs[0])
+            source_mac = mac_str_to_binary(self.int_to_info[intf].mac_addr)
+            target_mac = mac_str_to_binary("00:00:00:00:00:00")
+            opcode     = b'\x00\x01'
+
+            print("source_ip:  ", source_ip,  " ", type(source_ip))
+            print("source_mac: ", source_mac, " ", type(source_mac))
+            print("dest_ip:    ", next_hop,   " ", type(next_hop))
+            print("dest_mac:   ", target_mac, " ", type(target_mac))
+            print("opcode:     ", opcode,     " ", type(opcode))
+
+            self.send_arp_message(source_ip, source_mac, ip_str_to_binary(next_hop), target_mac, opcode, intf)
+
 
     def send_packet(self, pkt):
         print(f'Attempting to send packet:\n{repr(pkt)}')
